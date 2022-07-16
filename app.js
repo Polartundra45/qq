689912e8089d3298fd12baa9518de4c4bcddcb85
Adrian Losoya <adrianlosoya@Adrians-MacBook-Air.local>
2022-07-15 19:30:30 -0700

    setting ejs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-app.set('view engine', 'views');
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
