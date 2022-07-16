215ad6b83119e04cc0b78156815d825d0a5cecb1
Adrian Losoya <adrianlosoya@Adrians-MacBook-Air.local>
2022-07-15 19:17:30 -0700

    add more info

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
 	res.send('its still working i am amazing bro yo');
 });
 
+app.get('/about', (req,res) => {
+	res.send('this is the about page')
+})
+
 app.listen(port, () => {
 	console.log(`listening of port ${port}`);
 });
