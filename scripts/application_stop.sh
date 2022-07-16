26e83feb3eb0c3b550c7105d3b68d9ba87154080
Adrian Losoya <adrianlosoya@Adrians-MacBook-Air.local>
2022-07-15 18:54:39 -0700

    add application stop file

---
+++ scripts/application_stop.sh
@@ -0,0 +1,9 @@
+#Give permission for everything in the express-app directory
+sudo chmod -R 777 /home/ec2-user/personal-node-app
+
+#go into working directory
+cd /home/ec2-user/personal-node-app
+
+#stopping app
+
+pm2 stop app.js
