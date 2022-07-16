80634320febe160e29323c6440dac9e271d104c3
Adrian Losoya <adrianlosoya@Adrians-MacBook-Air.local>
2022-07-15 19:11:45 -0700

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('its working');
+	res.send('its still working i am amazing bro');
 });
 
 app.listen(port, () => {
